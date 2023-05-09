bbf5eb5ce53e1dd81c45de6f6bc46688a885f4d4
Thai Esseà <thaisacontar@gmail.com>
2023-05-09 17:36:55 -0400

    thai: add folder src/controllers

---
+++ src/controllers/CategController.ts
@@ -0,0 +1,169 @@
+import { Category } from "@prisma/client";
+import { Request, Response } from "express";
+import CategService from "../services/CategService";
+
+class CategController {
+
+    static async create(req: Request, res: Response) {
+
+        try {
+
+            const { title, content, user_id, category_status_id } = req.body;
+
+            if (!title || !content || !user_id || !category_status_id) {
+                return res
+                    .status(500)
+                    .json({ success: false, message: "✖️ Você precisa informar todos os campos necessários para criar a Categoria!" })
+            }
+
+            const category: Category | string = await CategService.createCategory({
+                title,
+                content,
+                user_id,
+                category_status_id
+            } as Category); // forçando um objeto a ser do tipo 'Category'
+
+            if (typeof category === 'string') return res
+                .status(500)
+                .json({ success: false, message: category });
+
+            return res.json({
+                success: true,
+                message: "🎉 Categoria criada com sucesso!",
+                result: category
+            })
+
+        } catch (error) {
+            console.log(error);
+            return res
+                .status(500)
+                .json({ success: false, message: "✖️ Ops, tente novamente!" });
+        }
+    }
+
+    static async index(req: Request, res: Response) {
+
+        try {
+            const categories = await CategService.getCategories();
+
+            return res.json({
+                success: true,
+                result: categories
+            })
+
+        } catch (error) {
+            console.log(error);
+
+            return res
+                .status(500)
+                .json({ success: false, message: "✖️ Ops, Tente novamente!" });
+        }
+    }
+
+    static async show(req: Request, res: Response) {
+        try {
+            const { id } = req.params;
+
+            if (!id) return res
+                .status(500)
+                .json({ success: false, message: "✖️ É obrigatório informar o ID da categoria!" });
+
+            if (isNaN(Number(id))) return res
+                .status(500)
+                .json({ success: false, message: "✖️ O ID precisa ser um número!" });
+
+            const category = await CategService.getCategory(Number(id));
+            if (!category) return res
+                .status(500)
+                .json({ success: false, message: "✖️ Categoria não encontrada para o ID informado!" });
+
+            return res.json({
+                success: true,
+                result: category
+            });
+
+        } catch (error) {
+            console.log(error);
+
+            return res
+                .status(500)
+                .json({ success: false, message: "✖️ Ops, tente novamente!" });
+        }
+    }
+
+    static async update(req: Request, res: Response) {
+        try {
+            const { id } = req.params;
+
+            if (!id) return res
+                .status(500)
+                .json({ success: false, message: "✖️ É obrigatório informar o ID da categoria!" });
+
+            if (isNaN(Number(id))) return res
+                .status(500)
+                .json({ success: false, message: "✖️ O ID precisa ser um número!" });
+
+            const { title, content, user_id, category_status_id } = req.body;
+
+            if (!title && !content && !user_id && !category_status_id) {
+                return res
+                    .status(500)
+                    .json({ success: false, message: "✖️ Você precisa preencher pelo menos um campo para atualizar a Categoria!" });
+            }
+
+            const category: Category | string = await CategService.updateCategory(Number(id), title, content, user_id, category_status_id);
+
+            if (typeof category === 'string') return res
+                .status(404)
+                .json({ success: false, message: category });
+
+            return res.json({
+                success: true,
+                message: "Categoria atualizada com sucesso!",
+                result: category
+            });
+
+        } catch (error) {
+            console.log(error);
+
+            return res
+                .status(500)
+                .json({ success: false, message: "✖️ Ops, tente novamente!" });
+        }
+    }
+
+    static async delete(req: Request, res: Response) {
+
+        try {
+
+            const { id } = req.params;
+
+            if (!id) return res
+                .status(500)
+                .json({ success: false, message: "✖️ É obrigatório informar o ID da categoria!" });
+
+            if (isNaN(Number(id))) return res
+                .status(500)
+                .json({ success: false, message: "✖️ O ID precisa ser um número!" });
+
+            const category = await CategService.deleteCategory(Number(id));
+
+            if (typeof category === 'string') return res
+                .status(404)
+                .json({ success: false, message: category });
+
+            return res.json({
+                success: true,
+                message: "✔️ Categoria deletada com sucesso!"
+            });
+
+        } catch (error) {
+            console.log(error);
+
+            return res
+                .status(500)
+                .json({ success: false, message: "✖️ Ops, tente novamente!" });
+        }
+    }
+}
+export default new CategController();
