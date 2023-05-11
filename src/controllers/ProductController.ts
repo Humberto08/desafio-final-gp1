bb329a15f0d1b2f0b9d5154705d31fc8172ebfa6
Thai Esseà <thaisacontar@gmail.com>
2023-05-11 19:53:24 -0400

    conflitos

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -167,4 +167,5 @@ class ProductController {
         }
     }
 }
+
 export default ProductController;
--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,15 @@
 import { Application, Request, Response } from "express";
 import { Router } from "./helpers/Router";
 import UserController from "./controllers/UserController";
+import CategController from "./controllers/CategController";
+import ProductController from "./controllers/ProductController";
 
 export function setupRoutes(app: Application) {
+
     const router = new Router(app);
 
     router.get("/", (req: Request, res: Response) => {
-        res.send(" 🚀 Aplicação iniciada com sucesso!");
+        res.status(200).json("🚀 Aplicação iniciada com sucesso!");
     })
 
     router.group("/users", (router) => {
@@ -16,4 +19,20 @@ export function setupRoutes(app: Application) {
         router.put("/:id", UserController.update);
         router.delete("/:id", UserController.delete);
     })
+
+    router.group("/categorias", (router) => {
+        router.post("/", CategController.create);
+        router.get("/", CategController.index);
+        router.get("/:id", CategController.show);
+        router.put("/:id", CategController.update)
+        router.delete("/:id", CategController.delete)
+    });
+
+    router.group("/produtos", (router) => {
+        router.post("/", ProductController.create);
+        router.get("/", ProductController.index);
+        router.get("/:id", ProductController.show);
+        router.put("/:id", ProductController.update)
+        router.delete("/:id", ProductController.delete)
+    });
 }
