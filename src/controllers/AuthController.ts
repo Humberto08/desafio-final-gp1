47c6a987731abf54209c31613be433431022adfd
Humberto Luciano <103616315+Humberto08@users.noreply.github.com>
2023-05-27 12:09:59 -0300

    Logout

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -5,37 +5,47 @@ import { sign } from 'jsonwebtoken';
 
 
 class AuthController {
-    
 
-        static async authenticate(req: Request, res: Response, next: NextFunction) {
 
-            const { email, password } = req.body;
+    static async authenticate(req: Request, res: Response, next: NextFunction) {
 
-            const user = await prisma.user.findUnique({
-                where: {
-                    email
-                }
-            });
+        const { email, password } = req.body;
 
-            if (!user) {
-                return res.json({ error: "Usuário não encontrado" })
+        const user = await prisma.user.findUnique({
+            where: {
+                email
             }
+        });
+
+        if (!user) {
+            return res.json({ error: "Usuário não encontrado" })
+        }
 
 
-            const isValuePasssword = await compare(password, user.password)
+        const isValuePasssword = await compare(password, user.password)
 
-            if (!isValuePasssword) {
-                return res.json({ error: "Senha incorreta" })
-            }
+        if (!isValuePasssword) {
+            return res.json({ error: "Senha incorreta" })
+        }
 
-            const token = sign({ id: user.id }, "secret", { expiresIn: "1h" });
-
-
-            return res.json({ user, token })
+        const token = sign({ id: user.id }, "secret", { expiresIn: "1h" });
 
 
+        return res.json({ user, token })
+
+
+    }
+
+    static async logout(req: Request, res: Response, next: NextFunction) {
+        try {
+            res.clearCookie("jsonwebtoken");
+            res.clearCookie("refreshtoken");
+            return res.sendStatus(200);
+        } catch (error) {
+            next(error);
         }
     }
+}
 
 
 
