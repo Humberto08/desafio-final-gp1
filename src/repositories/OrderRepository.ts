4a218b7644d75654b8e75afa0166117af71eda20
Thai Esseà <thaisacontar@gmail.com>
2023-06-12 08:02:26 -0400

    Ajustes em 'fechar pedido'

--- src/repositories/OrderRepository.ts
+++ src/repositories/OrderRepository.ts
@@ -12,51 +12,15 @@ class OrderRepository {
             }
         })
 
-        const userCart = await prisma.cart.findFirst({
-            where: { id: cart_id }
-        })
-
-        if (!userCart) {
+        if (!getUserCart) {
             return false
         }
-
-        let order: Order | Cart | null = null;
-
-        let total = getUserCart[0].total_value;
-
-        if (getUserCart.length > 0) {
-
-            order = getUserCart[0];
-
-        } else {
-
-            order = await prisma.order.create({
-                data: {
-                    user_id: cart_id,
-                    cart_id: cart_id,
-                    order_status: "Placed",
-                    total_value: total
-                }
-            })
-        }
-
-        if (!order) {
-            return false
-        }
-
-        await prisma.order.update({
-            where: {
-                id: order.id,
-            },
-            data: {
-                total_value: getUserCart[0].total_value,
-            }
-        })
 
         return await prisma.order.create({
             data: {
                 user_id: cart_id,
                 cart_id: cart_id,
+                order_status: "Placed",
                 total_value: getUserCart[0].total_value
             }
         })
